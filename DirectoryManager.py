f5cee061b29b8927e484e45c52dc3f6753fa8805
Matej2 <11059438+matej2@users.noreply.github.com>
2025-06-09 11:38:47 +0200

    File level output

--- DirectoryManager.py
+++ DirectoryManager.py
@@ -18,18 +18,25 @@ class DirectoryManager:
         dir_list = self.get_directories()
         structure = self.structure.get_structure().get("subdirectories")
 
-        self.check_subdirectories(dir_list, structure)
+        self.check_subdirectories(dir_list, structure, 0)
 
-    def check_subdirectories(self, dir_list: list[str], dir_structure: dict):
+    def check_subdirectories(self, dir_list: list[str], dir_structure: dict, level: int):
         for directory in dir_structure:
             dir_name = directory.get("name")
             dir_desc = directory.get("description")
             dir_subdir = directory.get("subdirectories")
+            dir_level = ""
+
+            i = 0
+            while i < level:
+                dir_level += "."
+                i += 1
 
             if dir_name in dir_list:
-                print(f'[+] {dir_name}: {dir_desc}')
+                print(f'{dir_level}[+] {dir_name}: {dir_desc}')
             else:
-                print(f'[-] {dir_name}: {dir_desc}')
+                print(f'{dir_level}[-] {dir_name}: {dir_desc}')
 
             if len(dir_subdir) > 0:
-                self.check_subdirectories(dir_list, dir_subdir)
+                self.check_subdirectories(dir_list, dir_subdir, (level+1))
+
