e809f271086e477f373aae714415a85444750485
Matej2 <11059438+matej2@users.noreply.github.com>
2025-06-09 11:06:03 +0200

    Recursive directory check

--- DirectoryManager.py
+++ DirectoryManager.py
@@ -12,17 +12,24 @@ class DirectoryManager:
         f = []
         for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
             f.extend(filenames)
-            break
         return f
 
     def check_directories(self):
         dir_list = self.get_directories()
+        structure = self.structure.get_structure().get("subdirectories")
 
-        for directory in self.structure.get_structure().get("subdirectories"):
+        self.check_subdirectories(dir_list, structure)
+
+    def check_subdirectories(self, dir_list: list[str], dir_structure: dict):
+        for directory in dir_structure:
             dir_name = directory.get("name")
             dir_desc = directory.get("description")
+            dir_subdir = directory.get("subdirectories")
 
             if dir_name in dir_list:
                 print(f'[+] {dir_name}: {dir_desc}')
             else:
                 print(f'[-] {dir_name}: {dir_desc}')
+
+            if len(dir_subdir) > 0:
+                self.check_subdirectories(dir_list, dir_subdir)
